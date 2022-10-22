2cbab08479af6de29f788a207f0c17f05f5b1c2b
Dominik Lewczyński <dominiklew200060@gmail.com>
2022-10-22 15:18:02 +0200

    Wykonanie zadania 10

--- osoby/admin.py
+++ osoby/admin.py
@@ -4,8 +4,10 @@ from .models import Druzyna, Osoba
 # Register your models here.
 class Admin(admin.ModelAdmin):
     list_display = ['imie', 'nazwisko', 'miesiac_urodzenia', "data_dodania", 'kraj']
+    list_filter = ['kraj', 'data_dodania']
 class DruzynaAdmin(admin.ModelAdmin):
     list_display = ['nazwa', 'kraj']
+    list_filter = ['kraj']
 
 admin.site.register(Osoba, Admin)
 admin.site.register(Druzyna, DruzynaAdmin)
